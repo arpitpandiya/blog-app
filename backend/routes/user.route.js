6f586328ba0a5ac5edd45a199d445889e089995a
arpit pandiya <arpitpandiya2003@gmail.com>
2025-03-03 18:37:29 +0530

    write api for user and admin profile

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { login, logout, register } from '../controller/user.controller.js';
+import { getAdmins, getMyProfile, login, logout, register } from '../controller/user.controller.js';
 import { isAuthenticated } from '../middleware/authUser.js';
 
 const router = express.Router()
@@ -7,4 +7,6 @@ const router = express.Router()
 router.post("/register", register);
 router.post("/login", login);
 router.get("/logout",isAuthenticated, logout);
+router.get("/my-profile",isAuthenticated, getMyProfile);
+router.get("/admins",getAdmins)
 export default router;
