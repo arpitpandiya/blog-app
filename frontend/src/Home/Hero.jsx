b954faec804e0e9cd82e3eaa75004b4a0e2fbc17
arpit pandiya <arpitpandiya2003@gmail.com>
2025-03-09 20:46:15 +0530

    update

--- frontend/src/Home/Hero.jsx
+++ frontend/src/Home/Hero.jsx
@@ -9,11 +9,10 @@ const Hero = () => {
     <div className="container max-w-screen-xl mx-auto my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-6">
       {blogs && blogs.length > 0 ? (
         blogs.slice(0, 4).map((element) => {
-          console.log(element);
 
           return (
             <Link
-              to={`/`}
+              to={`/blog/${element._id}`}
               key={element._id}
               className="bg-white rounded-lg hover:shadow-lg overflow-hidden transform hover:scale-105 transition-transform duration-300"
             >
@@ -21,15 +20,23 @@ const Hero = () => {
                 <img
                   src={element.blogImage.url}
                   alt=""
-                  className="w-full h-50 object-cover"
+                  className="w-full h-56 object-cover"
                 />
                 <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent opacity-75 group-hover:opacity-100 transition-transform duration-300"></div>
-                <h1 className="absolute bottom-4 left-4 text-white text-xl font-bold group-hover:text-yellow-500 transition-colors duration-300">{element.title}</h1>
+                <h1 className="absolute bottom-4 left-4 text-white text-xl font-bold group-hover:text-yellow-500 transition-colors duration-300">
+                  {element.title}
+                </h1>
               </div>
               <div className="p-6 flex items-center ">
-                <img src={element.adminPhoto} alt="" className="w-12 h-12 rounded-full border-2 border-yellow-400"/>
+                <img
+                  src={element.adminPhoto}
+                  alt=""
+                  className="w-12 h-12 rounded-full border-2 border-yellow-400"
+                />
                 <div className="ml-4">
-                  <p className="text-lg font-semibold text-gray-800">{element.adminName}</p>
+                  <p className="text-lg font-semibold text-gray-800">
+                    {element.adminName}
+                  </p>
                   <p className="text-xs text-gray-400">New</p>
                 </div>
               </div>
@@ -37,7 +44,9 @@ const Hero = () => {
           );
         })
       ) : (
-        <div></div>
+        <div className="flex h-screen items-center justify-center">
+          Loading....
+        </div>
       )}
     </div>
   );
