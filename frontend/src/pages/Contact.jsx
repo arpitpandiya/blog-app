f8b3d6a264c9d513c84802deeafa4ab73a91dd67
arpit pandiya <arpitpandiya2003@gmail.com>
2025-03-12 21:26:57 +0530

    create contact page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,115 @@
 import React from 'react'
+import { FaEnvelope, FaMapMarkerAlt, FaPhone } from 'react-icons/fa'
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import axios from 'axios';
 
 const Contact = () => {
+  const {register, handleSubmit, watch, formState: {errors},} = useForm();
+
+  const onSubmit = async(data)=>{
+    const userInfo ={
+      access_key: "696cb5b0-946a-42ff-af52-5b0586e59e51",
+      name: data.username,
+      email: data.email,
+      message: data.message,
+    };
+    try{
+      await axios.post("https://api.web3forms.com/submit", userInfo);
+      toast.success("Message sent successfully");
+    } catch(error){
+      toast.error("error occurred");
+    }
+  }
   return (
-    <div>Contact</div>
+    <div>
+    <div className="bg-gray-50 min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-4xl w-full space-y-8 bg-white p-10 rounded-lg shadow-lg">
+        <div className="text-center">
+          <h2 className="text-3xl font-extrabold text-gray-900">
+            Contact Us
+          </h2>
+        </div>
+        <div className="flex flex-col md:flex-row justify-between">
+          <div className="w-full md:w-1/2 mb-8 md:mb-0 md:pr-4">
+            <h3 className="text-lg font-medium text-gray-700 mb-4">
+              Send us a message
+            </h3>
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+              <div>
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="Your Name"
+                  className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-2 focus:border-blue-600"
+                  {...register("username", { required: true })}
+                />
+                {errors.username && (
+                  <span className="text-sm text-red-500 font-semibold">
+                    This field is required
+                  </span>
+                )}
+              </div>
+              <div>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Your Email"
+                  className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-2 focus:border-blue-600"
+                  {...register("email", { required: true })}
+                />
+                {errors.email && (
+                  <span className="text-sm text-red-500 font-semibold">
+                    This field is required
+                  </span>
+                )}
+              </div>
+              <div>
+                <textarea
+                  name="message"
+                  placeholder="Your Message"
+                  className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-2 focus:border-blue-600"
+                  {...register("message", { required: true })}
+                />
+                {errors.message && (
+                  <span className="text-sm text-red-500 font-semibold">
+                    This field is required
+                  </span>
+                )}
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="w-full bg-black text-white px-4 py-2 rounded-lg hover:bg-yellow-600 duration-300 "
+                >
+                  Send Message
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="w-full md:w-1/2 md:pl-4">
+            <h3 className="text-lg font-medium text-gray-700 mb-4">
+              Contact Information
+            </h3>
+            <ul className="space-y-4">
+              <li className="flex items-center space-x-2">
+                <FaPhone className="text-red-500" />
+                <span>+91 8949486397</span>
+              </li>
+              <li className="flex items-center space-x-2">
+                <FaEnvelope className="text-pink-500" />
+                <span>thearpitpandiya@gmail.com</span>
+              </li>
+              <li className="flex items-center space-x-2">
+                <FaMapMarkerAlt className="text-green-500" />
+                <span>Bhilwara, Rajasthan, India</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   )
 }
 
