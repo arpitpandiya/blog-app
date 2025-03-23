41a38858c2b690b0fefee66b37b4e8b4f0a80158
arpit pandiya <arpitpandiya2003@gmail.com>
2025-03-24 00:28:23 +0530

    add files

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -1,26 +1,50 @@
 import axios from "axios";
 import React, { createContext, useEffect, useContext, useState } from "react";
+import Cookies from "js-cookie";
 export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [blogs, setBlogs] = useState();
+  const [profile, setProfile] = useState();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  
   useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+          const { data } = await axios.get(
+            "http://localhost:4001/api/users/my-profile",
+            {
+              withCredentials: true,
+              headers: { "Content-Type": "application/json" },
+            }
+          );
+          console.log("API Response:", data);
+          setProfile(data);
+          setIsAuthenticated(true);
+        
+      } catch (error) {
+        console.log(error);
+      }
+    };
     const fetchBlogs = async () => {
       try {
-        const {data} = await axios.get(
-          "http://localhost:4001/api/blogs/all-blogs", 
-          {withCredentials: true}
+        const { data } = await axios.get(
+          "http://localhost:4001/api/blogs/all-blogs",
+          { withCredentials: true }
         );
-        console.log("API Response:",data);
+        console.log("API Response:", data);
         setBlogs(data);
       } catch (error) {
         console.log(error);
       }
     };
     fetchBlogs();
+    fetchProfile();
   }, []);
 
   return (
-    <AuthContext.Provider value={{ blogs }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ blogs, profile, isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
 
