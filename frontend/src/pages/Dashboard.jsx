909b5ece7942892de2e384cf1143e2dd7f9711ad
arpit pandiya <arpitpandiya2003@gmail.com>
2025-04-01 01:18:06 +0530

    fix bugs

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -5,11 +5,16 @@ import MyProfile from '../dashboard/MyProfile';
 import MyBlogs from '../dashboard/MyBlogs';
 import CreateBlog from '../dashboard/CreateBlog';
 import UpdateBlog from '../dashboard/UpdateBlog';
+import { Navigate } from 'react-router-dom';
 const Dashboard = () => {
   const{profile, isAuthenticated} = useAuth();
   const [component, setComponent] = useState("My Blogs");
   console.log(profile);
   console.log(isAuthenticated);
+
+  if(!isAuthenticated){
+    return <Navigate to={"/"}/>;
+  }
   return (
     <div>
       <div>
