f6e0d792d253d3353079be7f64fc865a25d5ea49
arpit pandiya <arpitpandiya2003@gmail.com>
2025-04-03 02:12:43 +0530

    fix bugs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,11 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider.jsx";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+  <Toaster/>
     <AuthProvider>
       <App />
     </AuthProvider>
