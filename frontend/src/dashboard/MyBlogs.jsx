fbd14939a70e3e5298405c42ab18ad5abaa89c86
arpit pandiya <arpitpandiya2003@gmail.com>
2025-04-03 02:12:01 +0530

    add files

--- frontend/src/dashboard/MyBlogs.jsx
+++ frontend/src/dashboard/MyBlogs.jsx
@@ -1,83 +1,93 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import toast from "react-hot-toast"
-import { Link } from 'react-router-dom';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { Link, useNavigate } from "react-router-dom";
 
 const MyBlogs = () => {
-  const[myBlogs, setMyBlogs] = useState([]);
-  useEffect(()=>{
-    const fetchMyBlogs = async()=>{
-      try{
-        const {data} = await axios.get("http://localhost:4001/api/blogs/my-blog",
-          {withCredentials: true});
-          console.log(data);
-          setMyBlogs(data);
-      } catch(error){
+  const navigateTo = useNavigate();
+  const [myBlogs, setMyBlogs] = useState([]);
+  console.log(myBlogs)
+  useEffect(() => {
+    const fetchMyBlogs = async () => {
+      try {
+        const { data } = await axios.get(
+          "http://localhost:4001/api/blogs/my-blog",
+          { withCredentials: true }
+        );
+        console.log(data);
+        setMyBlogs(data);
+      } catch (error) {
         console.log(error);
       }
     };
     fetchMyBlogs();
-  },[]);
+  }, []);
 
-  const handleDelete = async(id)=>{
-    await axios.delete(`http://localhost:4001/api/blogs/delete/${id}`,{withCredentials:true})
-    .then((res)=>{
-      toast.success(res.data.message || "Blog deleted successfully")
-      setMyBlogs((value)=>value.filter((blog)=>blog._id!==id))
-    }).catch((error)=>{
-      toast.error(error.response.message || "Failed to delete blog")
-    })
-  }
+  const handleDelete = async (id) => {
+    await axios
+      .delete(`http://localhost:4001/api/blogs/delete/${id}`, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        toast.success(res.data.message || "Blog deleted successfully");
+        navigateTo("/")
+        setMyBlogs((value) => value.filter((blog) => blog._id !== id));
+      })
+      .catch((error) => {
+        toast.error(error.response.message || "Failed to delete blog");
+      });
+  };
   return (
     <div>
-    <div className="container mx-auto my-12 p-4">
-      <div className="grid gap-6 md:grid-cols-1 lg:grid-cols-2 lg:ml-70 xl:grid-cols-3 md:ml-20">
-        {myBlogs && myBlogs.length > 0 ? (
-          myBlogs.map((element) => (
-            <div
-              className="bg-white shadow-lg rounded-lg overflow-hidden"
-              key={element._id}
-            >
-              {element?.blogImage && (
-                <img
-                  src={element?.blogImage.url}
-                  alt="blogImg"
-                  className="w-full h-48 object-cover"
-                />
-              )}
-              <div className="p-4">
-                <span className="text-sm text-gray-600">
-                  {element.category}
-                </span>
-                <h4 className="text-xl font-semibold my-2">
-                  {element.title}
-                </h4>
-                <div className="flex justify-between mt-4">
-                  <Link
-                    to={`/blog/update/${element._id}`}
-                    className="text-blue-500 bg-white rounded-md shadow-lg px-3 py-1 border border-gray-400 hover:underline"
-                  >
-                    UPDATE
-                  </Link>
-                  <button
-                    onClick={() => handleDelete(element._id)}
-                    className="text-red-500 bg-white rounded-md shadow-lg px-3 py-1 border border-gray-400 hover:underline"
-                  >
-                    DELETE
-                  </button>
+      <div className="container mx-auto my-12 p-4">
+        <div className="grid gap-6 md:grid-cols-1 lg:grid-cols-2 lg:ml-70 xl:grid-cols-3 md:ml-20">
+          {myBlogs && myBlogs.length > 0 ? (
+            myBlogs.map((element) => (
+              <Link
+                to={`/blog/${element._id}`}
+                className="bg-white shadow-lg rounded-lg overflow-hidden"
+                key={element._id}
+              >
+                {element?.blogImage && (
+                  <img
+                    src={typeof element.blogImage === "string" ? element.blogImage : element.blogImage?.url}
+                    alt="blogImg"
+                    className="w-full h-48 object-cover"
+                  />
+                )}
+                <div className="p-4">
+                  <span className="text-sm text-gray-600">
+                    {element.category}
+                  </span>
+                  <h4 className="text-xl font-semibold my-2">
+                    {element.title}
+                  </h4>
+                  <div className="flex justify-between mt-4">
+                    <Link
+                      to={`/blog/update/${element._id}`}
+                      className="text-blue-500 bg-white rounded-md shadow-lg px-3 py-1 border border-gray-400 hover:underline"
+                    >
+                      UPDATE
+                    </Link>
+                    <button
+                      onClick={() => handleDelete(element._id)}
+                      className="text-red-500 bg-white rounded-md shadow-lg px-3 py-1 border border-gray-400 hover:underline"
+                    >
+                      DELETE
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </div>
-          ))
-        ) : (
-          <p className="text-center text-gray-500">
-            You have not posted any blog to see!
-          </p>
-        )}
+              </Link>
+            ))
+          ) : (
+            <p className="text-center text-gray-500">
+              You have not posted any blog to see!
+            </p>
+          )}
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default MyBlogs
+export default MyBlogs;
